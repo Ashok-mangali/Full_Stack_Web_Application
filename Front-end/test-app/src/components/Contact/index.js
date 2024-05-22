ea35318dab1814666fa2c611ab5985531d8516a2
Ashok <aashok59238@gmail.com>
2024-05-22 10:57:49 +0530

    Update index.js

--- Front-end/test-app/src/components/Contact/index.js
+++ Front-end/test-app/src/components/Contact/index.js
@@ -4,8 +4,8 @@ function Contact() {
   return (
     <div className='App'>
         <h1>Let's Meet</h1>
-        <h3>Contact Us: <span>support@nextenti.ai</span></h3>
-        <h3>Address: <span>3rd floor, Plot No. 21, Madhapur, Gafoornagar, Hyderabad, Telangana 500081</span></h3>
+        <h3>Contact Us: <span>aashok59238@gmail.com</span></h3>
+        <h3>Address: <span>Anantapur, Andhra Pradesh 515402</span></h3>
 
     </div>
   
