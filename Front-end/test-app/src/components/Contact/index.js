d7f7cf03191cbf84d0cf3aaf2ce9585a7ec86581
Ashok <aashok59238@gmail.com>
2024-05-22 10:59:00 +0530

    Update index.js

--- Front-end/test-app/src/components/Contact/index.js
+++ Front-end/test-app/src/components/Contact/index.js
@@ -5,7 +5,7 @@ function Contact() {
     <div className='App'>
         <h1>Let's Meet</h1>
         <h3>Contact Us: <span>aashok59238@gmail.com</span></h3>
-        <h3>Address: <span>Anantapur, Andhra Pradesh 515402</span></h3>
+        <h3>Address: <span>Anantapur, Andhra Pradesh - 515402</span></h3>
 
     </div>
   
